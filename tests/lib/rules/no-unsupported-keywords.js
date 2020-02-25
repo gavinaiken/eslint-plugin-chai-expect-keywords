a5c60b915ffd25354731f7e39f36e78a0e65bebe
Brett Zamir <brettz9@yahoo.com>
2020-02-25 08:05:16 +0800

    - Enhancement: Add `allowChaiDOM` option (and as part of `recommended`) - Testing: Add invalid versions of the option tests - Maintenance: Add `.editorconfig`

--- tests/lib/rules/no-unsupported-keywords.js
+++ tests/lib/rules/no-unsupported-keywords.js
@@ -104,6 +104,14 @@ ruleTester.run('no-unsupported-keywords with options', rule, {
         expect(result).to.eventually.be.ok;
       });
     `
+  }, {
+    options: [{allowChaiDOM: true}],
+    code: `
+    it("works as expected", function() {
+      expect(result).to.have.text('abc');
+      expect(result).not.be.displayed;
+    });
+    `
   }],
 
   invalid: [{
@@ -116,6 +124,51 @@ ruleTester.run('no-unsupported-keywords with options', rule, {
     `,
     errors: [{
       message: '"to.bar.be.ok" contains unknown keyword "bar"'
+    }]
+  }, {
+    code: `
+      it("works as expected", function() {
+        expect(result).to.be.foo();
+        expect(result).to.zing.be.ok;
+      });
+    `,
+    errors: [{
+      message: '"to.be.foo" contains unknown keyword "foo"'
+    }, {
+      message: '"to.zing.be.ok" contains unknown keyword "zing"'
+    }]
+  }, {
+    code: `
+      it("works as expected", function() {
+        expect(result).to.have.returned(foo);
+        expect(result).to.have.been.called;
+      });
+    `,
+    errors: [{
+      message: '"to.have.returned" contains unknown keyword "returned"'
+    }, {
+      message: '"to.have.been.called" contains unknown keyword "called"'
+    }]
+  }, {
+    code: `
+      it("works as expected", function() {
+        expect(result).to.eventually.be.ok;
+      });
+    `,
+    errors: [{
+      message: '"to.eventually.be.ok" contains unknown keyword "eventually"'
+    }]
+  }, {
+    code: `
+    it("works as expected", function() {
+      expect(result).to.have.text('abc');
+      expect(result).not.be.displayed;
+    });
+    `,
+    errors: [{
+      message: '"to.have.text" contains unknown keyword "text"'
+    }, {
+      message: '"not.be.displayed" contains unknown keyword "displayed"'
     }]
   }]
 });
